6a0b9c4d8c3faf3888a92dec223f3f5cef25399c
AayushGD2005 <aaayushdinesh@gmail.com>
2026-06-08 10:28:02 +0530

    Update medrep.java

--- src/main/java/com/tracker/demo/repository/medrep.java
+++ src/main/java/com/tracker/demo/repository/medrep.java
@@ -1,8 +1,8 @@
-package com.tracker.demo.repository;
+// package com.tracker.demo.repository;
 
-import com.tracker.demo.model.medicine;
-import org.springframework.data.jpa.repository.JpaRepository;
-import com.tracker.demo.model.medicine;
+// import com.tracker.demo.model.medicine;
+// import org.springframework.data.jpa.repository.JpaRepository;
+// import com.tracker.demo.model.medicine;
 
-public interface medrep extends JpaRepository<medicine, Long> {
-}
+// public interface medrep extends JpaRepository<medicine, Long> {
+// }
