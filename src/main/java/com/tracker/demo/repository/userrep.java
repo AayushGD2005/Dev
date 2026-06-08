2e3af3b75b9b55aa9cd7f5dcf2ac2af834d11fc3
AayushGD2005 <aaayushdinesh@gmail.com>
2026-06-08 10:28:23 +0530

    Update userrep.java

--- src/main/java/com/tracker/demo/repository/userrep.java
+++ src/main/java/com/tracker/demo/repository/userrep.java
@@ -1,8 +1,8 @@
-package com.tracker.demo.repository;
+// package com.tracker.demo.repository;
 
-import org.springframework.data.jpa.repository.JpaRepository;
-import com.tracker.demo.model.user;
+// import org.springframework.data.jpa.repository.JpaRepository;
+// import com.tracker.demo.model.user;
 
-public interface userrep extends JpaRepository<user, Long> {
+// public interface userrep extends JpaRepository<user, Long> {
 
-}
+// }
