d61d9386544cb7cbc9fe5933e420ae2113be49fa
AayushGD2005 <aaayushdinesh@gmail.com>
2026-06-08 10:16:30 +0530

    Update MedicineController.java

--- src/main/java/com/tracker/demo/controller/MedicineController.java
+++ src/main/java/com/tracker/demo/controller/MedicineController.java
@@ -1,42 +1,61 @@
 package com.tracker.demo.controller;
 
-import com.tracker.demo.model.medicine;
-import com.tracker.demo.repository.medrep;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.*;
-
+import java.util.ArrayList;
 import java.util.List;
+
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.tracker.demo.model.medicine;
 
 @RestController
 @RequestMapping("/api")
 @CrossOrigin
 public class MedicineController {
 
-    @Autowired
-    private medrep repo;
+    private List<medicine> medicines = new ArrayList<>();
+    private Long currentId = 1L;
 
     @PostMapping("/add")
     public medicine addMedicine(@RequestBody medicine m) {
-        return repo.save(m);
+        m.setId(currentId++);
+        medicines.add(m);
+        return m;
     }
 
     @GetMapping("/all")
     public List<medicine> getAll() {
-        return repo.findAll();
+        return medicines;
     }
 
     @DeleteMapping("/delete/{id}")
     public String deleteMedicine(@PathVariable Long id) {
-        repo.deleteById(id);
+        medicines.removeIf(m -> m.getId().equals(id));
         return "Deleted";
     }
+
     @PutMapping("/taken/{id}")
     public medicine markTaken(@PathVariable Long id) {
-        medicine m = repo.findById(id).orElse(null);
-        if (m != null) {
-            m.setStatus("Taken");
-            return repo.save(m);
+
+        for (medicine m : medicines) {
+            if (m.getId().equals(id)) {
+                m.setStatus("Taken");
+                return m;
+            }
         }
+
         return null;
+    }
+
+    @GetMapping("/")
+    public String home() {
+        return "Medicine Reminder System Running Successfully";
     }
 }
